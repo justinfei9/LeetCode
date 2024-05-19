2d625ae7030d181def7f41264f4f577585e96f3a
justinfei9 <jpf99@yahoo.com>
2024-05-18 23:24:55 -0400

    1422. Maximum Score After Splitting a String
    here is my solution for leet code question 1422

---
+++ 1422MaximumScoreAfterSplittingaString.java
@@ -0,0 +1,27 @@
+class 1422MaximumScoreAfterSplittingaString {
+    public int maxScore(String s) {
+        int max = 0;
+        int ones =0;
+        int zeroes=0;
+        for(int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c == '1')
+            {
+                ones++;
+            }
+        }
+        for(int i=0; i<s.length()-1; i++)
+        {
+            char c = s.charAt(i);
+            if(c == '0')
+            {
+                zeroes++;
+            }else{
+                ones--;
+            }
+            max =Math.max((zeroes+ones),max);
+        }
+        return max;
+    }
+}
